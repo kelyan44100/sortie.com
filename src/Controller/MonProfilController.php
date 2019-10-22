f673dd6a892e3025d8941b2ec0c8cf058ad1eba6
Administrateur <patcha.danet@gmail.com>
2019-10-22 16:45:18 +0200

    Commit mon profil+modifier2

---
+++ src/Controller/MonProfilController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\MonProfilType;
+use App\Repository\UserRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MonProfilController extends Controller
+{
+    /**
+     * @Route("/monProfil", name="mon_profil")
+     */
+    public function monProfil(){
+        $user = $this->getUser();
+        return $this->render('mon_profil/monProfil.html.twig', ['user'=>$user] );
+    }
+
+
+    /**
+     * @Route("/monProfil/update/{id}", name="mon_profil_update", requirements={"id":"\d+"})
+     */
+    public function updateMonProfil(Request $request, ObjectManager $manager, UserRepository $repo, User $u){
+
+        $user = $repo->find($u);
+        //$nom_site->setNomSite($user->getSite());
+        $formMonProfil = $this->createForm(MonProfilType::class,$user);
+        $formMonProfil->handleRequest($request);
+        if ($formMonProfil->isSubmitted() && $formMonProfil->isValid()){
+            $error=false;
+            if(!$error){
+                $manager->persist($user);
+                $manager->flush();
+                $this->addFlash('success', 'Your profile successfully updated!' );
+                return $this->redirectToRoute('mon_profil');
+            }else{
+                $this->addFlash('warning', 'One errors occurred, on profile update!' );
+            }
+        }
+        return $this->render('mon_profil/updateMonProfil.html.twig', [
+            'formMonProfil'=>$formMonProfil->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/monProfil/file/{id}", name="mon_profil_file")
+     */
+    public function fichier(User $u){
+        $dir = $this->getParameter('download_dir');
+        if(strlen(trim($u->getFile())) > 0 && file_exists($dir . $u->getFile())) {
+
+            // this is needed to safely include the file name as part of the URL
+            $safeFilename = \transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $idea->getTitle());
+
+            $f = explode('.', $u->getFile());
+            $extension = strtolower($f[count($f) - 1]);
+
+            $nameFile = $safeFilename.'.'.$extension;
+
+            $file = new File($dir . $u->getFile());
+
+            return $this->file($file, $nameFile);
+        }
+        else{
+            throw $this->createNotFoundException("File not found");
+        }
+
+    }
+}
---
+++ src/Form/MonProfilType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Site;
+use App\Entity\User;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class MonProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pseudo', TextType::class, ['label'=>'Pseudo'])
+            ->add('prenom', TextType::class, ['label'=>'Prenom'])
+            ->add('nom', TextType::class, ['label'=>'Nom'])
+            ->add('telephone', TelType::class, ['label'=>'Téléphone'])
+            ->add('email',EmailType::class, ['label'=>'Email'])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Confirmation'),
+                'invalid_message' => 'Your passwords do not match!',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('site', EntityType::class, [
+                'class'=> Site::class,
+                //Attribut utilisé pour l'affichage
+                'choice_label' => 'site',
+                //Fait une requête particulière
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('c')
+                        ->orderBy('c.site');
+                }
+            ])
+            ->add('fileTemp', FileType::class, [
+            'label'=> 'Ma photo',
+            'required' => false,
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
