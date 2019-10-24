439de13f8d2cd80094b1da60612c8999e671757b
Administrateur <sophie.sennoun2018@campus-eni.fr>
2019-10-24 10:49:01 +0200

    Affichage et ajout des villes

---
+++ src/Controller/AjoutVilleController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ville;
+use App\Form\VilleType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AjoutVilleController extends Controller
+{
+
+    /**
+     * @Route("/ville/ajout", name="ville_ajout")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+
+    public function add(Request$request, EntityManagerInterface $em){
+
+        // Récupération des villes déjà existantes pour les afficher
+        $repo = $em->getRepository(Ville::class);
+        $villes = $repo->findAll();
+
+        // Gestion du formulaire
+        $ville = new Ville();
+
+        $form = $this->createForm(VilleType::class, $ville);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($ville);
+            $em->flush();
+            return $this->redirectToRoute('ville_ajout', array('villes'=>$villes));
+
+        }
+
+
+        return $this->render('ajout_ville/ajoutVille.html.twig', [
+            'villes' => $villes,
+            'villeForm' => $form->createView()
+        ]);
+
+
+    }
+
+    /**
+     * @Route("/ville/supp/{id}", name="ville_supp")
+     * @param Ville $ville
+     * @param EntityManagerInterface $em
+     * @return RedirectResponse
+     */
+
+    public function delete(Ville $ville, EntityManagerInterface $em){
+
+        $em->remove($ville);
+        $em->flush();
+        $this->addFlash("success", "La ville a bien été supprimée de notre base");
+        return $this->redirectToRoute('ville_ajout');
+
+    }
+
+}
--- src/Form/VilleType.php
+++ src/Form/VilleType.php
@@ -2,27 +2,28 @@
 
 namespace App\Form;
 
-use App\Entity\Site;
+use App\Entity\Ville;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SiteType extends AbstractType
+class VilleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-               $builder
-                   ->add('nom_site', TextType::Class, ['label'=>'Nouveau site'
-                       ]);
-
+        $builder
+            ->add('nom_ville', TextType::Class, ['label' => 'Ville'
+            ])
+            ->add('code_postal', TextType::Class, ['label' => 'Code postal'
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Site::class,
-
+            'data_class' => Ville::class,
         ]);
     }
 }
