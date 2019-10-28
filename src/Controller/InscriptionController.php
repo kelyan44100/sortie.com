b4691c52672ded34335e7e6343b01b4603aabc51
Administrateur <sophie.sennoun2018@campus-eni.fr>
2019-10-28 18:29:47 +0100

    Gestion bouton s'inscrire (à un evenement) qui fonctioooonnnne

---
+++ src/Controller/InscriptionController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Inscription;
+use App\Entity\Sortie;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InscriptionController extends Controller
+{
+
+
+    /**
+     * @Route("/sorties/inscription/{id}", name="sortie_inscription", requirements={"id": "\d+"})
+     * @param EntityManagerInterface $em
+     * @param Sortie $sortie
+     * @return RedirectResponse
+     * @throws Exception
+     */
+    public function subscribe(Sortie $sortie,EntityManagerInterface $em ){
+
+        $inscription = new Inscription();
+        //participant (utilisateur courant)
+        $participant = $this->getUser();
+        //date d'inscription (aujourd'hui)
+        $today = (new \DateTime('now'))->setTime(0,0,0);
+
+
+        $inscription->setSortie($sortie);
+        $inscription->setParticipant($participant);
+        $inscription->setDateInscription($today);
+
+        //update de la sortie
+        $nbInscriptions = $sortie->getNbInscription();
+        $sortie->setNbInscription($nbInscriptions+1);
+
+        $em->persist($inscription);
+        $em->flush();
+
+        $this->addFlash("success", "Inscription réussie !");
+
+        return $this->redirectToRoute('sortie_list');
+
+    }
+
+    /**
+     * @Route("/sorties/desinscription/{id}", name="sortie_desinscription", requirements={"id": "\d+"})
+     * @param Sortie $sortie
+     * @param EntityManagerInterface $em
+     * @return RedirectResponse
+     */
+
+    public function unsubscribe(Sortie $sortie, EntityManagerInterface $em){
+        //participant (utilisateur courant)
+        $participant = $this->getUser();
+        foreach ($sortie->getInscriptions() as $inscription){
+            if($participant == $inscription->getParticipant()){
+                $em->remove($inscription);
+                $em->flush();
+            }
+        }
+
+        $this->addFlash("success", "Désistement réussi");
+        return $this->redirectToRoute('sortie_list');
+    }
+
+}
