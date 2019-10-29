c2aee08663cefc3420af6ebb8bee31436c41547d
kélyan <kelyan.appolinaire2019@campus-eni.fr>
2019-10-29 16:00:21 +0100

    regex

--- src/Entity/Site.php
+++ src/Entity/Site.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -19,6 +20,7 @@ class Site
 
     /**
      * @ORM\Column(type="string", length=30)
+     * @Assert\Regex("/^[[:alpha:]]([-' ]?[[:alpha:]])*$/", message="Entrer un site valide!")
      */
     private $nom_site;
 
